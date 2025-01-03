0b0d2e08efae09cd0607c2f784abf0aebdd988a4
Yawar Abbas <139193021+yawar788@users.noreply.github.com>
2025-01-03 23:40:23 +0500

    Update dom.js

--- dom.js
+++ dom.js
@@ -1,6 +1,5 @@
 // What is DOM ? 
 
-
 // The Document Object Model (DOM) in JavaScript is a programming interface for web documents. 
 // It represents the structure of a document as a tree of objects that can be manipulated via 
 // JavaScript. Each element in the HTML becomes an object that can be accessed and modified to
