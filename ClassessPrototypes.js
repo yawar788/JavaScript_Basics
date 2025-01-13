ea113025713764983c22a5aa19de5b22108daeb5
Yawar Abbas <139193021+yawar788@users.noreply.github.com>
2025-01-13 03:22:19 -0800

    Update ClassessPrototypes.js
    Prototypal Inheritace and Class based-Inheritance

--- ClassessPrototypes.js
+++ ClassessPrototypes.js
@@ -36,3 +36,84 @@ person2.greet(); // Output: Hello, my name is Abbas and I am 30 years old.
 // Classes
 // JavaScript classes, introduced in ECMAScript 2015 (ES6), are a syntactical sugar over the existing prototype-based 
 // inheritance. They make it easier to understand and write the object-oriented code.
+
+// Example using classes
+// Let's rewrite the previous example using classes:
+
+class Person{
+    constructor(name,age){
+        this.name=name;
+        this.age=age;
+    }
+
+    greet(){
+        console.log(`Hi there, this is ${this.name} and my age is ${this.age}`)
+    }
+};
+
+let person1=new Person('Yawar Abbas',12);
+let person2=new Person('Abbas',13);
+
+person1.greet();
+person2.greet();
+
+// In this example, the Person class defines a constructor for initializing new objects and a greet method.
+// Inheritance
+// Both prototypes and classes support inheritance.
+
+// Prototypal inheritance
+// Constructor function
+function Animal() {}
+
+// Adding method to the prototype
+Animal.prototype.eat = function() {
+    console.log('Eating...');
+};
+
+// Constructor function
+function Dog(name) {
+    this.name = name;
+}
+
+// Inheriting from Animal
+Dog.prototype = Object.create(Animal.prototype);
+
+// Adding method to the prototype
+Dog.prototype.bark = function() {
+    console.log('Woof!');
+};
+
+// Creating an instance
+const spot = new Dog('Spot');
+
+// Using the methods from the prototype
+spot.eat(); // Output: Eating...
+spot.bark(); // Output: Woof!
+
+
+// Class-based inheritance
+// Defining a base class
+class Animal {
+    eat() {
+        console.log('Eating...');
+    }
+}
+
+// Defining a subclass
+class Dog extends Animal {
+    constructor(name) {
+        super(); // Call the parent class constructor
+        this.name = name;
+    }
+
+    bark() {
+        console.log('Woof!');
+    }
+}
+
+// Creating an instance
+const spot = new Dog('Spot');
+
+// Using the methods
+spot.eat(); // Output: Eating...
+spot.bark(); // Output: Woof!
