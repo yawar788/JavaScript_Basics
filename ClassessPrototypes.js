b26779ccc5f92baea637f510fdc1b3ca20363469
Yawar Abbas <139193021+yawar788@users.noreply.github.com>
2025-01-13 02:18:15 -0800

    Update ClassessPrototypes.js
    classes

--- ClassessPrototypes.js
+++ ClassessPrototypes.js
@@ -2,6 +2,8 @@
 
 // Classes and prototypes are fundamental concepts in JavaScript, ensuring reusability and maintainability
 // of your code.
+// In JavaScript, every object has a prototype (also an object) from which it inherits properties and methods.
+// This is the core of JavaScript's inheritance model.
 
 // Prototypes in JavaScript
 // JavaScript is prototype-based, meaning that object properties and methods can be shared through prototypes.
@@ -21,12 +23,16 @@ Person.prototype.greet = function() {
 };
 
 // Creating instances of Person
-let person1 = new Person("Yawar", 25);
+let person1 = new Person("Yawar Abbas", 25);
 let person2 = new Person("Abbas", 30);
 
 // Using the greet method
-person1.greet(); // Output: Hello, my name is Yawar and I am 25 years old.
+person1.greet(); // Output: Hello, my name is Yawar Abbas and I am 25 years old.
 person2.greet(); // Output: Hello, my name is Abbas and I am 30 years old.
 
 // In this example, we define a Person constructor function and add a greet method to its prototype. Both instances,
 // person1 and person2, can use this method.
+
+// Classes
+// JavaScript classes, introduced in ECMAScript 2015 (ES6), are a syntactical sugar over the existing prototype-based 
+// inheritance. They make it easier to understand and write the object-oriented code.
